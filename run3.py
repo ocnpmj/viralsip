bbeea6625bf5c90e698646429f459d83130db536
ikopmniko <hannapitman786@gmail.com>
2025-05-06 22:16:31 +0700

    Update run3.py

--- run3.py
+++ run3.py
@@ -26,7 +26,7 @@ start_row =0  # Baris pertama (0-based index)
 end_row = 145  # Baris terakhir yang ingin diproses
 
 # Deklarasi akun tunggal
-email = "diczkuethien"
+email = "VivianInthong"
 password = "@@Eskepal123"
 
 # Baca judul video sesuai rentang yang diinginkan
